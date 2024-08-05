44e0052bc80da56cf117af838eeebb9348e5ae3f
Jack <jack@hopsoffice.com>
2024-08-05 21:53:36 +0900

    make times return a Dollar instance

--- mod_test.ts
+++ mod_test.ts
@@ -3,6 +3,8 @@ import { Dollar } from "./mod.ts";
 
 Deno.test(function testMultiplication() {
   const five = new Dollar(5);
-  five.times(2);
-  assertEquals(10, five.amount);
+  let product: Dollar = five.times(2);
+  assertEquals(10, product.amount);
+  product = five.times(3);
+  assertEquals(15, product.amount);
 });

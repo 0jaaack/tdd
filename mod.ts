198a621c298b38032a8152dc3e411e35710a4999
Jack <jack@hopsoffice.com>
2024-08-05 22:20:35 +0900

    create Money class and make Dollar class extends Money

--- mod.ts
+++ mod.ts
@@ -1,7 +1,11 @@
-export class Dollar {
+class Money {
+}
+
+export class Dollar extends Money {
   private amount: number;
 
   constructor(amount: number) {
+    super();
     this.amount = amount;
   }
 

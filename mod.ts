cc3036e19548e7ffb4f06bc445e9b37f347a0cb1
Jack <jack@hopsoffice.com>
2024-08-05 22:15:19 +0900

    add franc multiplication test

--- mod.ts
+++ mod.ts
@@ -13,3 +13,19 @@ export class Dollar {
     return this.amount === doller.amount;
   }
 }
+
+export class Franc {
+  private amount: number;
+
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+
+  times(multiplier: number) {
+    return new Franc(this.amount * multiplier);
+  }
+
+  equals(franc: Franc) {
+    return this.amount === franc.amount;
+  }
+}
--- mod_test.ts
+++ mod_test.ts
@@ -1,5 +1,5 @@
 import { assert, assertEquals, assertFalse } from "@std/assert";
-import { Dollar } from "./mod.ts";
+import { Dollar, Franc } from "./mod.ts";
 
 Deno.test(function testMultiplication() {
   const five = new Dollar(5);
@@ -10,4 +10,10 @@ Deno.test(function testMultiplication() {
 Deno.test(function testEquality() {
   assert(new Dollar(5).equals(new Dollar(5)));
   assertFalse(new Dollar(5).equals(new Dollar(6)));
+});
+
+Deno.test(function testFrancMultiplication() {
+  const five = new Franc(5);
+  assertEquals(new Franc(10), five.times(2));
+  assertEquals(new Franc(15), five.times(3));
 });
